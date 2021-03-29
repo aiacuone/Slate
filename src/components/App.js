20ac24870a097707f4634924724ce123091fdb94
aiacuone <aiacuone@users.noreply.github.com>
2021-03-29 22:36:18 +0100

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Editor from '../components/Editor'
+import Editor2 from '../components/Editor2'
+
+export default function App() {
+	return (
+		<div>
+			{/* <Editor /> */}
+			<Editor2/>
+		</div>
+	)
+}
